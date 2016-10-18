bac039505dba8f2b84addaff77a8311ab266eddd
cole <cole.brumley@gmail.com>
2016-10-18 10:44:55 -0500

    Make py2/3 switchable

--- profile.d/py3_venv.sh
+++ profile.d/py3_venv.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
-export VIRTUAL_ENV="$HOME/py3.5"
-export PATH="$VIRTUAL_ENV/bin:$PATH"
-unset PYTHON_HOME
+
+PY3_PATH="$HOME/py3.5"
+PY2_PATH="$HOME/py2.7"
+
+setpy(){
+    [ "$1" == "3" ] && VIRTUAL_ENV="$PY3_PATH"
+    [ "$1" == "2" ] && VIRTUAL_ENV="$PY2_PATH"
+
+    PATH="$VIRTUAL_ENV/bin:$PATH"
+    export VIRTUAL_ENV PATH
+    unset PYTHON_HOME
+}
+
+setpy 3
