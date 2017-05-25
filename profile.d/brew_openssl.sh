009cc9908d3314418ef42505f71282cade7cb712
Cole Brumley <cole.brumley@gmail.com>
2017-05-25 15:21:22 -0500

    Update openssl links

--- profile.d/brew_openssl.sh
+++ profile.d/brew_openssl.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 link_openssl_stuff(){
-    ln -s /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib /usr/local/lib/
-    ln -s /usr/local/opt/openssl/lib/libssl.1.0.0.dylib /usr/local/lib/
-    ln -s /usr/local/Cellar/openssl/1.0.2j/bin/openssl "$HOME/bin/openssl"
+    ln -s /usr/local/opt/openssl/lib/libcrypto.dylib /usr/local/lib/
+    ln -s /usr/local/opt/openssl/lib/libssl.dylib /usr/local/lib/
+    ln -s /usr/local/Cellar/openssl/1.0.2l/bin/openssl "$HOME/bin/openssl"
 }
 
 cfg_openssl() {
     if ! [[ "$(brew ls --versions|grep ^openssl|awk '{print $2}')" =~ ^1.0.2 ]];
     then brew update && brew install openssl; fi
 
-    [[ -h /usr/local/lib/libcrypto.1.0.0.dylib ]] || link_openssl_stuff
+    [[ -h /usr/local/lib/libcrypto.dylib ]] || link_openssl_stuff
 }
 
 is_installed brew && cfg_openssl
