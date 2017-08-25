75df71216e016d9947fea217f60288f2aef4b391
Cole Brumley <cole.brumley@33727-mbpr.2d.hq>
2017-08-25 08:17:16 -0500

    Add explanation for setpy

--- profile.d/py3_venv.sh
+++ profile.d/py3_venv.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# I use setpy as a replacement for virtualenv's bin/activate script.
+# setpy is expecting python virtualenv directories to be in the home
+# directory and named like ~/pyVERSION (i.e. ~/py2, ~/py3.6, etc).
+# Then, you just need to use `setpy VERSION` to enable a specific version
+# without all the bin/activate garbage.
+
 setpy(){
     for d in $HOME/py*/bin; do 
         PATH="${PATH//$d:}"
