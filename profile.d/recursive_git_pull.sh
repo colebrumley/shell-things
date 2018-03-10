585a6b2602ccf910b335aacf10be7898ab96b9a1
Cole Brumley <cole@systemc.lan>
2018-03-10 10:44:40 -0600

    IDK why my local shellcheck isn't catching these

--- profile.d/recursive_git_pull.sh
+++ profile.d/recursive_git_pull.sh
@@ -2,6 +2,7 @@
 
 rpull() {
     local basedir=${1}
+    # shellcheck disable=SC2016
     find "$basedir" -type d -name .git \
         -exec sh -c 'd="$(dirname "$1")";echo "Pulling $d";cd "$d" && git pull' _ {} \;
 }
