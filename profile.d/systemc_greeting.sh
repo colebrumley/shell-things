254b4e3b23698f0f62ea003d2c554080560c2eb6
Cole Brumley <cole@systemc.local>
2018-10-22 12:31:17 -0500

    add a sort -u

--- profile.d/systemc_greeting.sh
+++ profile.d/systemc_greeting.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # figlet -f sblood "$(hostname -s)" | lolcat --force | head -n -1
 echo -e "Date\\t\\t$(date)"
-echo -e "Default IP\\t$(ip route show | grep ^default | awk '{print $3}')"
+echo -e "Default IP\\t$(ip route show | grep ^default | awk '{print $3}' | sort -u)"
 echo -e "Disk Usage\\t$(df -h --total | tail -n 1 | awk '{print $5}')"
 echo -e "Python Version\\t$(python -V | awk '{print $2}')"
