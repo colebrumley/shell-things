623e3de2e128872dd537ffa059be42d51e07d85a
Cole Brumley <cole@brumley.io>
2020-03-11 09:21:01 -0500

    Update to new method to match brew's naming

--- profile.d/osx_py3_venv.sh
+++ profile.d/osx_py3_venv.sh
@@ -30,22 +30,33 @@ pypip_update(){
 export -f pypip_update
 
 pyupdate_brew(){
-    for ver in $(brew list --versions | grep "^python" | awk '{$1=""; print $0}'); do
-        local major_version=${ver:0:1}
-        if [[ -d "$HOME/lib/py$ver" ]]; then
-            echo Python "$ver" is already configured
-        else
-            if [[ $major_version == 2 ]]; then
-                src_str="/usr/local/Cellar/python@$major_version/$ver/bin/python$major_version"
-            else
-                src_str="/usr/local/Cellar/python$major_version/$ver/bin/python$major_version"
-            fi
-            /usr/local/bin/virtualenv -p "$src_str" "$HOME/lib/py$ver"
-            ln -s "$HOME/lib/py$ver" "$HOME/py$ver"
-            setpy "$ver"
-            pip install -r "$HOME/lib/bak/python${major_version}_pip_freeze.txt"
-        fi
+    for ver in /usr/local/Cellar/python*; do
+        read -r FULL_PATH < <(find "$ver" -perm 755 -type l -name python -exec readlink -f {} \;)
+        read -r VERSION < <($FULL_PATH -V | awk '{print $2}')
+        [[ -z $VERSION ]] && continue
+        [[ -d $HOME/py$VERSION ]] && continue
+        /usr/local/bin/virtualenv -p "$FULL_PATH" "$HOME/lib/py$VERSION"
+        ln -s "$HOME/lib/py$VERSION" "$HOME/py$VERSION"
+        setpy "$VERSION"
+        pip install -r "$HOME/lib/bak/python${VERSION:0:1}_pip_freeze.txt"
     done
+
+    # for ver in $(brew list --versions | grep "^python" | awk '{$1=""; print $0}'); do
+    #     local major_version=${ver:0:1}
+    #     if [[ -d "$HOME/lib/py$ver" ]]; then
+    #         echo Python "$ver" is already configured
+    #     else
+    #         if [[ $major_version == 2 ]]; then
+    #             src_str="/usr/local/Cellar/python@$major_version/$ver/bin/python$major_version"
+    #         else
+    #             src_str="/usr/local/Cellar/python$major_version/$ver/bin/python$major_version"
+    #         fi
+    #         /usr/local/bin/virtualenv -p "$src_str" "$HOME/lib/py$ver"
+    #         ln -s "$HOME/lib/py$ver" "$HOME/py$ver"
+    #         setpy "$ver"
+    #         pip install -r "$HOME/lib/bak/python${major_version}_pip_freeze.txt"
+    #     fi
+    # done
 }
 export -f pyupdate_brew
 
