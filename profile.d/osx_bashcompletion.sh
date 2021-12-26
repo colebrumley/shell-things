f895cc5a1259356d016805c6948159981d2a2c0c
Cole Brumley <cole@brumley.io>
2021-12-25 19:19:35 -0600

    only load bash completion in bash

--- profile.d/osx_bashcompletion.sh
+++ profile.d/osx_bashcompletion.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if is_installed brew; then
-    if [[ -d "$BREW_PREFIX"/etc/bash_completion.d ]]; then
+    if [[ -d "$BREW_PREFIX"/etc/bash_completion.d ]] && [[ $SHELL =~ /bash$ ]]; then
         # shellcheck disable=SC1090
         for f in "$BREW_PREFIX"/etc/bash_completion.d/*; do . "$f"; done
     fi
